606819337fc7e20fbf69ae1a56c18d44da2d7c4c
Rainer Stropek <rainer@timecockpit.com>
2024-10-10 08:38:58 +0200

    Saved in TypeScript Playground at 2024-10-10T06:38:57.732Z

--- dice/index.ts
+++ dice/index.ts
@@ -7,7 +7,7 @@ function setup() {
 
   // Generate an integer random number 
   // between 1 (including) and 7 (excluding)
-  const dice = Math.floor(random(4, 6));
+  const dice = Math.floor(random(1, 7));
 
   noStroke();
   fill("white");
@@ -20,21 +20,28 @@ function setup() {
     circle(width / 4, height / 4, dicePointDiameter);
     circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
   } else if (dice === 3) {
-  circle(width / 4, height / 4, dicePointDiameter);
-  circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
-  circle(width / 2, height / 2, dicePointDiameter);
-}else if (dice==4){
-  circle(width / 4, height / 4, dicePointDiameter);
-  circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
-  circle(3*width / 4, height / 4, dicePointDiameter);
-  circle( width / 4,  3*height / 4, dicePointDiameter);
-}else if(dice==5){
-  circle(width / 4, height / 4, dicePointDiameter);
-  circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
-  circle(3*width / 4, height / 4, dicePointDiameter);
-  circle( width / 4,  3*height / 4, dicePointDiameter);
-  circle(width / 2, height / 2, dicePointDiameter);
-}
+    circle(width / 4, height / 4, dicePointDiameter);
+    circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
+    circle(width / 2, height / 2, dicePointDiameter);
+  } else if (dice == 4) {
+    circle(width / 4, height / 4, dicePointDiameter);
+    circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
+    circle(3 * width / 4, height / 4, dicePointDiameter);
+    circle(width / 4, 3 * height / 4, dicePointDiameter);
+  } else if (dice == 5) {
+    circle(width / 4, height / 4, dicePointDiameter);
+    circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
+    circle(3 * width / 4, height / 4, dicePointDiameter);
+    circle(width / 4, 3 * height / 4, dicePointDiameter);
+    circle(width / 2, height / 2, dicePointDiameter);
+  } else if (dice == 6) {
+    circle(width / 4, height / 4, dicePointDiameter);
+    circle(3 * width / 4, 3 * height / 4, dicePointDiameter);
+    circle(3 * width / 4, height / 4, dicePointDiameter);
+    circle(width / 4, 3 * height / 4, dicePointDiameter);
+    circle(3*width/4,height/2,dicePointDiameter)
+    circle(width/4,height/2,dicePointDiameter)
+  }
 
 
 }
