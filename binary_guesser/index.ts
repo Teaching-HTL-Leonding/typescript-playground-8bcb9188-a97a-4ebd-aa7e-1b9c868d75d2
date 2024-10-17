7f0321572235d32426c4111fbf6af6afc4ad760f
Rainer Stropek <rainer@timecockpit.com>
2024-10-17 09:38:30 +0200

    Saved in TypeScript Playground at 2024-10-17T07:38:29.825Z

--- binary_guesser/index.ts
+++ binary_guesser/index.ts
@@ -1,28 +1,22 @@
-  let binaryDigit1: number = 0
-  let binaryDigit2: number = 0
-  let binaryDigit3: number = 0
-  let binaryDigit4: number = 0
+let binaryDigit1: number = 0
+let binaryDigit2: number = 0
+let binaryDigit3: number = 0
+let binaryDigit4: number = 0
+let num: number;
 function setup() {
+  num = Math.floor(random(0, 15))
+
   createCanvas(500, 300)
   background("black")
-  const num = Math.floor(random(0, 15));
 
-  const lastDigit1 = num % 2;
-  const numWithoutLastDigit = Math.floor(num / 2);
-  const lastDigit2 = numWithoutLastDigit % 2;
-  const numWithoutLastDigit1 = Math.floor(numWithoutLastDigit / 2);
-  const lastDigit3 = numWithoutLastDigit1 % 2;
-  const numWithoutLastDigit2 = Math.floor(numWithoutLastDigit1 / 2);
-  const lastDigit4 = numWithoutLastDigit2 % 2;
-  const numWithoutLastDigit3 = Math.floor(numWithoutLastDigit2 / 2);
-  const numWithoutLastDigit4 = Math.floor(numWithoutLastDigit3 / 2);
   textAlign(CENTER, CENTER);
   fill("yellow");
   noStroke();
-  text(`${num} in binary?`, width / 2, height - 20);
+  textSize(30)
+  text(`${num} in binary?`, 250, 50);
   strokeWeight(1)
   textSize(40)
-  
+
   text(binaryDigit4, 337, 150)
   text(binaryDigit3, 274, 150)
   text(binaryDigit2, 211, 150)
@@ -30,34 +24,66 @@ function setup() {
 
   noFill()
   stroke("yellow")
-  strokeWeight(3)
+  strokeWeight(1)
   rect(2 * width / 8, 100, 50, 100)
   rect(3 * width / 8, 100, 50, 100)
   rect(4 * width / 8, 100, 50, 100)
   rect(5 * width / 8, 100, 50, 100)
 }
 function mouseClicked() {
+  strokeWeight(1)
 
   if (mouseX < 2 * width / 8 + 50 && mouseX > 2 * width / 8 && mouseY > 100 && mouseY < 200) {
-  fill("black")
-   rect(2 * width / 8, 100, 50, 100)
-   fill("yellow")
-    if(binaryDigit1===0){
-      binaryDigit1=1
-    }
-    else if(binaryDigit1===1){binaryDigit1=0}
-  text(binaryDigit1, 148, 150)
+    fill("black")
+    rect(2 * width / 8, 100, 50, 100)
+    fill("yellow")
+    if (binaryDigit1 === 0) {
+      binaryDigit1 = 1
+    } else { binaryDigit1 = 0 }
+    text(binaryDigit1, 148, 150)
   }
-    if (mouseX < 3 * width / 8 + 50 && mouseX > 3 * width / 8 && mouseY > 100 && mouseY < 200) {
-    if(binaryDigit2===0){
-      binaryDigit2=1
-    }
+  if (mouseX < 3 * width / 8 + 50 && mouseX > 3 * width / 8 && mouseY > 100 && mouseY < 200) {
+    fill("black")
+    rect(3 * width / 8, 100, 50, 100)
+    fill("yellow")
+    if (binaryDigit2 === 0) {
+      binaryDigit2 = 1
+    } else { binaryDigit2 = 0 }
+    text(binaryDigit2, 211, 150)
+  }
+  if (mouseX < 4 * width / 8 + 50 && mouseX > 4 * width / 8 && mouseY > 100 && mouseY < 200) {
+    fill("black")
+    rect(4 * width / 8, 100, 50, 100)
+    fill("yellow")
+    if (binaryDigit3 === 0) {
+      binaryDigit3 = 1
+    } else { binaryDigit3 = 0 }
+    text(binaryDigit3, 274, 150)
+  }
+  if (mouseX < 5 * width / 8 + 50 && mouseX > 5 * width / 8 && mouseY > 100 && mouseY < 200) {
+    fill("black")
+    rect(5 * width / 8, 100, 50, 100)
+    fill("yellow")
+    if (binaryDigit4 === 0) {
+      binaryDigit4 = 1
+    } else { binaryDigit4 = 0 }
+    text(binaryDigit4, 337, 150)
+  }
+  let decimalnum = binaryDigit1 * Math.pow(2, 3) + binaryDigit2 * Math.pow(2, 2) + binaryDigit3 * Math.pow(2, 1) + binaryDigit4 * Math.pow(2, 0)
+  if (decimalnum === num) {
+    background("black")
+    noFill()
+    stroke("green")
+    rect(2 * width / 8, 100, 50, 100)
+    rect(3 * width / 8, 100, 50, 100)
+    rect(4 * width / 8, 100, 50, 100)
+    rect(5 * width / 8, 100, 50, 100)
+    fill("green")
+      text(binaryDigit4, 337, 150)
+  text(binaryDigit3, 274, 150)
   text(binaryDigit2, 211, 150)
-  }
-    if (mouseX < 2 * width / 8 + 50 && mouseX > 2 * width / 8 && mouseY > 100 && mouseY < 200) {
-    if(binaryDigit1===0){
-      binaryDigit1=1
-    }
   text(binaryDigit1, 148, 150)
+    text(`${num} in binary?`, 250, 50);
+    text(`Correct!`,200,300)
   }
 }
