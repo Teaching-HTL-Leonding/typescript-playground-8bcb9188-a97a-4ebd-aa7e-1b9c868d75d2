65d78ad2b4d9739d55565fd1fc55140c69729a84
Rainer Stropek <rainer@timecockpit.com>
2024-11-06 11:23:32 +0100

    Saved in TypeScript Playground at 2024-11-06T10:23:31.590Z

---
+++ circle_tool_exam/index.ts
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(400, 200);
+    background("black");
+    angleMode(DEGREES);
+}
+function mouseMoved(){
+    ellipse(width/2,height/2,mouseX,mouseY)
+}
+// <<< Add the function `mouseMoved` with the required code here
