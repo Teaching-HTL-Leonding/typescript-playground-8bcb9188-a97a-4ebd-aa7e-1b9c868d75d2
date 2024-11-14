31dd23972c3e14c8f102903dac70c8f5f5dd55ab
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 09:31:28 +0100

    Saved in TypeScript Playground at 2024-11-14T08:31:27.648Z

--- pixel_x/index.ts
+++ pixel_x/index.ts
@@ -1,26 +1,26 @@
 function setup() {
     const SIZE = 425; // Canvas size
     const GRID = 25;  // Size of the squares
-let xyellow=GRID
-let yyellow=GRID
-let xgreen=SIZE-GRID*2
-let ygreen=GRID
+    let xyellow = GRID
+    let yyellow = GRID
+    let xgreen = SIZE - GRID * 2
+    let ygreen = GRID
     createCanvas(SIZE, SIZE);
     background("black");
 
     noStroke();
-while(xyellow<SIZE-GRID&&yyellow<SIZE-GRID){
-    fill("yellow")
-    rect(xyellow,yyellow,GRID,GRID)
-    xyellow+=25
-    yyellow+=GRID
-}
+    while (xyellow < SIZE - GRID && yyellow < SIZE - GRID) {
+        fill("yellow")
+        rect(xyellow, yyellow, GRID, GRID)
+        xyellow += 25
+        yyellow += GRID
+    }
 
-while(xgreen>=GRID&&ygreen<SIZE-GRID){
-    fill("lime")
-    rect(xgreen,ygreen,GRID,GRID)
-    xgreen-=25
-    ygreen+=GRID
-}
+    while (xgreen >= GRID && ygreen < SIZE - GRID) {
+        fill("lime")
+        rect(xgreen, ygreen, GRID, GRID)
+        xgreen -= 25
+        ygreen += GRID
+    }
     // <<< Add your code here
 }
