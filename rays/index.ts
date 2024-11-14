e970081456cdc5813ef0bfff1211eef9bd739af4
Rainer Stropek <rainer@timecockpit.com>
2024-11-14 09:31:34 +0100

    Saved in TypeScript Playground at 2024-11-14T08:31:33.758Z

--- rays/index.ts
+++ rays/index.ts
@@ -1,6 +1,6 @@
 const SIZE = 400;  // Canvas size (square)
 const MARGIN = 25; // Margin between the edges and the rays.
-                   // This is also the distance between the rays.
+// This is also the distance between the rays.
 
 function setup() {
   createCanvas(SIZE, SIZE);
@@ -10,15 +10,15 @@ function setup() {
 function mouseMoved() {
   background("black");
   strokeWeight(2);
-  let i=MARGIN
-while(i<SIZE){
-  stroke("green")
-line(i,MARGIN,mouseX,mouseY)
-line(i,SIZE-MARGIN,mouseX,mouseY)
-stroke("yellow")
-line(SIZE-MARGIN,i,mouseX,mouseY)
-line(MARGIN,i,mouseX,mouseY)
-i+=MARGIN
-}
+  let i = MARGIN
+  while (i < SIZE) {
+    stroke("green")
+    line(i, MARGIN, mouseX, mouseY)
+    line(i, SIZE - MARGIN, mouseX, mouseY)
+    stroke("yellow")
+    line(SIZE - MARGIN, i, mouseX, mouseY)
+    line(MARGIN, i, mouseX, mouseY)
+    i += MARGIN
+  }
   // <<< Add your code here
 }
