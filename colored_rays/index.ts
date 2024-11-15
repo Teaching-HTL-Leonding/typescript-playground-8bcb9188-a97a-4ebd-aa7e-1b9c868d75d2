ce205c992677ba77d453f477f12e9677f4fa8e6e
Rainer Stropek <rainer@timecockpit.com>
2024-11-15 12:27:25 +0100

    Saved in TypeScript Playground at 2024-11-15T11:27:25.553Z

--- colored_rays/index.ts
+++ colored_rays/index.ts
@@ -1,9 +1,9 @@
 const SIZE = 400;    // Canvas size (square)
 const MARGIN = 25; // Margin between the edges and the rays.
-   let raycolor=0                
-                    // This is also the distance between the rays.
+let raycolor = 0
+// This is also the distance between the rays.
 function setup() {
-colorMode(HSB)
+  colorMode(HSB)
   createCanvas(SIZE, SIZE);
   background("black");
 }
@@ -15,11 +15,11 @@ function draw() {
   // <<< Add your code here
   let i = MARGIN;
   while (i <= SIZE - MARGIN) {
-    stroke(raycolor,100,100);
+    stroke(raycolor, 100, 100);
     line(MARGIN, i, mouseX, mouseY);
     line(width - MARGIN, i, mouseX, mouseY);
 
-raycolor=( raycolor + 0.25)%360
+    raycolor = (raycolor + 0.25) % 360
 
     line(i, MARGIN, mouseX, mouseY);
     line(i, height - MARGIN, mouseX, mouseY);
