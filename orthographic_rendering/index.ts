b4cc42e1c4519f989d7939e3ff6e8dc8fed8c6bb
Rainer Stropek <rainer@timecockpit.com>
2025-02-27 09:15:40 +0100

    Saved in TypeScript Playground at 2025-02-27T08:15:39.676Z

---
+++ orthographic_rendering/index.ts
@@ -0,0 +1,61 @@
+const RAILROAD_WIDTH = 225;
+const RAILROAD_HEIGHT = 160;
+
+const WAGON_WIDTH = 160;
+const WAGON_HEIGHT = 113;
+
+let railroad: p5.Image;
+let train: p5.Image;
+const BASE_URL = "https://cddataexchange.blob.core.windows.net/images/trains";
+
+
+function preload() {
+
+    railroad = loadImage(`${BASE_URL}/railroad-straight.png`);
+    train = loadImage(`${BASE_URL}/train-carriage-wood.png`);
+}
+function setup() {
+    createCanvas(800, 550);
+    // <<< Load the image of a train wagon (train-carriage-wood.png)
+}
+
+function draw() {
+    background("#f0f0f0");
+
+    scale(0.5, 0.5);
+
+    // Move the origin to the right, upper corner. This makes sense
+    // because we must draw the images from right to left so that
+    // objects that are further left are "on top" of objects on
+    // the right.
+    translate(850, -100);
+
+    // <<< For task 2, add another loop here
+
+    // Draw five railroad segments
+    for (let k = 0; k < 3; k++) {
+
+        for (let i = 0; i < 5; i++) {
+            drawRailroad(i);
+        }
+        for (let i = 0; i < 6; i++) {
+            drawTrain(i)
+        }
+        translate(125, 100)
+    }
+
+    // <<< Add code to draw six train wagons
+}
+
+/** Draw a railroad segment */
+function drawRailroad(ix: number) {
+    image(railroad, -RAILROAD_WIDTH * ix, RAILROAD_HEIGHT * ix, railroad.width, railroad.height);
+
+}
+function drawTrain(ix: number) {
+
+    image(train, -WAGON_WIDTH * ix, WAGON_HEIGHT * ix, train.width, train.height);
+
+}
+
+// <<< Add a function that draws a train wagon in analogy to drawRailroad
